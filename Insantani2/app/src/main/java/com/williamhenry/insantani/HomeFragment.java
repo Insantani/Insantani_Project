ea9bfd73e17a3f012d33f8f20e20979db4b7192d
Agung Wirayogi <agungwy@gmail.com>
2015-10-22 21:49:31 +0700

    Front end: + Article data + clickable article with data - Search Bar
    Back end
    None

--- Insantani2/app/src/main/java/com/williamhenry/insantani/HomeFragment.java
+++ Insantani2/app/src/main/java/com/williamhenry/insantani/HomeFragment.java
@@ -71,11 +71,66 @@ public class HomeFragment extends Fragment {
         // specify an adapter (see also next example)
 
         ArrayList<Article> articles = new ArrayList<Article>();
+        articles.add(new Article(
+                "by Agung Wirayogi", "Chili for your healt",
+                "Chilies are excellent source of Vitamin, A, B, C and E with minerals like molybdenum, manganese, folate, potassium, thiamin, and copper. Chili contains seven times more vitamin C than orange.\n" +
+                        "\n" +
+                        "Ever since its introduction to India in 1498, chilies have been included in Ayurvedic medicines and used as " +
+                        "tonic to ward off many diseases. Chilies are good for slimming down as it burns the calorie easily. " +
+                        "Chilies stimulate the appetite, help to clear the lungs, and stimulate digestive system", R.mipmap.cabe
 
-        for (int i = 0; i < 20 ; i++){
-            articles.add(new Article(
-                    "Author" + i, "Title" + i, "This is about desc "+ i, R.drawable.sample_4 ));
-        }
+        ));
+        articles.add(new Article(
+                "by William Henry", "What is potato",
+                "According to the USDA National Nutrient Database, one half cup of shredded cabbage" +
+                        " (75 grams) contains 17 calories, 4 grams of carbohydrate (including 1 gram " +
+                        "of fiber and 2 grams of sugar) and 1 gram of protein. ", R.mipmap.col
+
+        ));
+        articles.add(new Article(
+                "by Izhar Almizan", "Advantage of carrot",
+                "Western culture’s  understanding of carrots being “good for the eyes” is one of the few we got right. " +
+                        "Carrots are rich in beta-carotene, which is converted into vitamin A in the liver. Vitamin A is " +
+                        "transformed in the retina, to rhodopsin, a purple pigment necessary for night vision.\n" +
+                        "\n" +
+                        "Read more: http://www.care2.com/greenliving/10-benefits-of-carrots.html#ixzz3pImoFOIR\n", R.mipmap.carrot
+
+        ));
+        articles.add(new Article(
+                "by Diovi Adzhalia", "Why my friend buy potato",
+                "Followers of low-carb diets give regular potatoes a bad rap. They are not exactly good for you when " +
+                        "dropped into a deep fryer, but potatoes are packed with powerful nutrients and antioxidants—compounds " +
+                        "that fight free radicals. The ORAC value (a measure of the total antioxidants in 100 grams) for a" +
+                        " medium baked potato with skin is a healthy 1,680, while that of a baked sweet potato with skin " +
+                        "is 766. Compare those with the values for carrots, either cooked (317) or raw (666)"
+                , R.mipmap.kentang
+
+        ));
+        articles.add(new Article(
+                "by Abicantya Sophie", "Tomato is good for you",
+                "Whether you refer to a tomato as a fruit or a vegetable, there is no doubt that a tomato " +
+                        "is a nutrient-dense, super-food that most people should be eating more of.\n" +
+                        "\n" +
+                        "The tomato has been referred to as a \"functional food,\" a food that goes beyond" +
+                        " providing just basic nutrition, additionally preventing chronic disease and" +
+                        " delivering other health benefits, due to beneficial phytochemicals such as lycopene."
+                , R.mipmap.tomat
+
+        ));
+        articles.add(new Article(
+                "by Yohana Kanisia", "Cucumber is cool",
+                "Cucumbers belong to the same plant family as squash, pumpkin, and " +
+                        "watermelon (the Cucurbitaceae family). Like watermelon, cucumbers " +
+                        "are made up of mostly (95 percent) water, which means eating them on" +
+                        " a hot summer day can help you stay hydrated."
+                , R.mipmap.timun
+
+        ));
+
+//        for (int i = 0; i < 20 ; i++){
+//            articles.add(new Article(
+//                    "Author" + i, "Title" + i, "This is about desc "+ i, R.drawable.sample_4 ));
+//        }
         mAdapter = new MyAdapter(articles,getContext());
         mRecyclerView.setAdapter(mAdapter);
 
