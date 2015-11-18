017576b5bcc8fcdcb4d87947c972d50a46ba6efa
Diovi Azalia <dioviazaleea@gmail.com>
2015-11-18 14:47:10 +0700

    check out
    commit new check out

---
+++ Insantani2/app/src/main/java/com/williamhenry/insantani/Checkout.java
@@ -0,0 +1,24 @@
+package com.williamhenry.insantani;
+
+/**
+ * Created by dioviazalia on 11/18/15.
+ */
+public class Checkout {
+    String location;
+    int postcode;
+
+    public Checkout(String location, int postcode){
+        this.location = location;
+        this.postcode = postcode;
+
+    }
+
+    public String getLocation{
+        return location;
+    }
+
+    public int getPostcode{
+        return postcode;
+    }
+
+}
