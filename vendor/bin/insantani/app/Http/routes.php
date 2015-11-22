e767f0880d943f1310444216e0a0ea981e48204d
williamhenry94 <william.hidayat@gmail.com>
2015-11-22 17:25:31 +0700

    fixing issues

---
+++ vendor/bin/insantani/app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('api/products',['uses'=>'ProductController@products','middleware'=>'products']);
+Route::get('api/products/{id}', array('uses' => 'ProductController@productDetail','middleware'=>'products'))->where('id', '.+');
+Route::get('api/feed',['uses'=>'ArticleController@articles','middleware'=>'articles']);
+
+Route::get('api/feed/article/{id}', array('uses' => 'ArticleController@articleDetail','middleware'=>'articles'))->where('id', '.+');
